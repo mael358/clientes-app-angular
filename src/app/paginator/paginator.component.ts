ba70d2d67871f1dd44cf57d052ecf58896c95562
Marcos Velasquez <marcosv200007@gmail.com>
2020-07-13 19:35:33 -0600

    Añadiendo retoques al paginador

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
-  @Input() paginator: any;
+  @Input()
+  paginator: any;
 
   paginas: number[];
+
+  desde: number;
+  hasta: number;
 
 
   constructor() { }
 
   ngOnInit(): void {
-    this.paginas = new Array(this.paginator.totalPages).fill(0).map(
-      (_valor, indice) => indice + 1
-    );
+    this.initPaginator();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    let paginatorActualizado = changes['paginator'];
+
+    if (paginatorActualizado.previousValue){
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
+    this.desde = Math.min(Math.max(1, this.paginator.number - 4), this.paginator.totalPages - 5); // 6
+    this.hasta = Math.max(Math.min(this.paginator.totalPages, this.paginator.number + 4), 6); // 10
+
+    if (this.paginator.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor, indice) => indice + this.desde);   
+      //this.paginas = new Array(this.paginator.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    } else {
+      this.paginas = new Array(this.paginator.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    }
   }
 
 }
