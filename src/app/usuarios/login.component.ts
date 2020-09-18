b97472e61ef19145898dab47f548e959c41abb06
Marcos Velasquez <marcosv200007@gmail.com>
2020-09-18 17:59:33 -0600

    Creando clase de Usuario y integrando metodo a login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = 'Iniciar sesión';
+  usuario: Usuario;
 
-
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.password == null){
+      swal.fire('Error', 'Usuario o contraseña vacias!', 'error');
+      return;
+    }
   }
 
 }
