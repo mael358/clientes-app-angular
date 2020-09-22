3ff4983990a281d424e376148ecc370e9eb1021f
Marcos Velasquez <marcosv200007@gmail.com>
2020-09-21 20:33:04 -0600

    Implementando servicios para guardar token en sesion del navegador

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -8,7 +8,30 @@ import { Usuario } from './usuario';
 })
 export class AuthService {
 
+  private _usuario: Usuario;
+  private _token: string;
+
   constructor(private http: HttpClient) { }
+
+  public get usuario(): Usuario{
+    if(this._usuario != null){
+      return this._usuario;
+    } else if(this._usuario == null && sessionStorage.getItem('usuario') != null){
+      this._usuario = JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+      return this._usuario;
+    }
+    return new Usuario();
+  }
+
+  public get token(): string{
+    if(this._token != null){
+      return this._token;
+    } else if(this._token == null && sessionStorage.getItem('token') != null){
+      this._token = sessionStorage.getItem('token');
+      return this._token;
+    }
+    return null;
+  }
 
   login(usuario: Usuario): Observable<any> {
     const urlEndPoint = 'http://localhost:8080/oauth/token';
@@ -25,5 +48,29 @@ export class AuthService {
     params.set('password', usuario.password);
     console.log(params.toString());
     return this.http.post<any>(urlEndPoint, params.toString(), { headers: httpHeaders });
+  }
+
+  guardarUsuario(access_token: string): void{
+    let payload = this.obtenerPayload(access_token);
+    this._usuario = new Usuario();
+    this._usuario.nombre = payload.nombre;
+    this._usuario.apellido = payload.apellido;
+    this._usuario.email = payload.email;
+    this._usuario.username = payload.user_name;
+    this._usuario.roles = payload.authorities;
+    //guardando usuario en la sesion del navegador
+    sessionStorage.setItem('usuario', JSON.stringify(this._usuario));
+  }
+
+  obtenerPayload(access_token: string): any {
+    if(access_token != null){
+      return JSON.parse(atob(access_token.split(".")[1]));
+    }
+    return null;
+  }
+
+  guardarToken(access_token: string): void{
+    this._token = access_token;
+    sessionStorage.setItem('token', access_token);
   }
 }
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -30,10 +30,12 @@ export class LoginComponent implements OnInit {
 
     this.authService.login(this.usuario).subscribe(response => {
       console.log(response);
-      let payload = JSON.parse(atob(response.access_token.split(".")[1]));
-      console.log(payload);
+
+      this.authService.guardarUsuario(response.access_token);
+      this.authService.guardarToken(response.access_token);
+      let usuario = this.authService.usuario;
       this.router.navigate(['/clientes']);
-      swal.fire('¡Bienvenido!', `Hola ${payload.user_name}, has iniciado sesión con éxito!`, 'success');
+      swal.fire('¡Bienvenido!', `Hola ${usuario.username}, has iniciado sesión con éxito!`, 'success');
     });
   }
 
