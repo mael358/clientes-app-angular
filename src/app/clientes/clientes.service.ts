a856fa6c2e21eb32d9ed6daa9900e47db1d4f254
Marcos Velasquez <marcosv200007@gmail.com>
2020-09-27 16:17:33 -0600

    Validando sesion para ver si ya expiro la sesion.

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -28,6 +28,10 @@ export class ClientesService {
 
   private isNotAuthorized(e): boolean{
     if(e.status == 401){
+
+      if(this.authService.isAuthenticated()){
+        this.authService.logout();
+      }
       this.router.navigate(['/login']);
       return true;
     }
