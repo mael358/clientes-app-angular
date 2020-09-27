70fb983a668337c8d23639e55541f9bd496ba637
Marcos Velasquez <marcosv200007@gmail.com>
2020-09-27 16:42:12 -0600

    Añadiendo Guard para verificar autenticacion de roles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,14 +25,15 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter'
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
 import { AuthGuard } from './usuarios/guards/auth.guard';
+import { RoleGuard } from './usuarios/guards/role.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
   { path: 'directivas', component: DirectivaComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'clientes/page/:page', component: ClientesComponent },
-  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
-  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'} },
+  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'} },
   { path: 'login', component: LoginComponent }
 ];
 
