46ac16510c9784ffbdb4556492242c95c06585a6
Marcos Velasquez <marcosv200007@gmail.com>
2020-10-21 16:08:02 -0600

    Areglando ruta mal escrita de factura

--- src/app/facturas/detalle-factura.component.ts
+++ src/app/facturas/detalle-factura.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
-import { Factura } from '../models/factura';
+import { Factura } from './models/factura';
 
 
 @Component({
