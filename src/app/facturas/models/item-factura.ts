43d92df68d130c6d9323aca6a16442a7d8ffa603
Marcos Velasquez <marcosv200007@gmail.com>
2020-10-29 21:47:07 -0600

    Agregando metodo para calcular total de linea.

--- src/app/facturas/models/item-factura.ts
+++ src/app/facturas/models/item-factura.ts
@@ -4,4 +4,8 @@ export class ItemFactura {
   producto: Producto;
   cantidad: number = 1;
   total: number;
+
+  public calcularLinea(): number{
+    return this.cantidad * this.producto.precio;
+  }
 }
