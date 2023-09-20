6b647ee58063d15527daa6da72fe6769f55aab09
Marcos Velasquez <marcosv200007@gmail.com>
2023-09-19 21:20:16 -0600

    Mantenimiento a pagina de clientes

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_BACKEND= 'https://backend-spring-clientes-app.herokuapp.com';
+//export const URL_BACKEND= 'https://backend-spring-clientes-app.herokuapp.com';
+export const URL_BACKEND= 'https://spring-backend-mael358-e193d882ebdf.herokuapp.com';
